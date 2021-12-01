00c9bcd8927802106a4b7f54c6f90951511496e6
Jacob Tan <jacob.tan229@gmail.com>
2021-12-02 11:47:35 +1300

    Updated .env variable names

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -6,7 +6,7 @@ const cryptoNewsHeaders = {
   "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
 };
 
-const baseUrl = process.env.REACT_APP_CRYPTO_API_URL;
+const baseUrl = process.env.REACT_APP_NEWS_API_URL;
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
