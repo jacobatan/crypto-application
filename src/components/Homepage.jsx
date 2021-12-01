a31c16db703f2fbf08055a0506673eb5d22364ae
Jacob Tan <jacob.tan229@gmail.com>
2021-12-01 15:08:19 +1300

    fixed responsiveness

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,12 +5,13 @@ import { Link } from "react-router-dom";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 import { Cryptocurrencies, News } from "../components";
+import Loader from "./Loader";
 
 const { Title } = Typography;
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
-  if (isFetching) return "Loading...";
+  if (isFetching) return <Loader />;
   const globalStats = data?.data?.stats;
   return (
     <>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,6 +5,7 @@ import moment from "moment";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 import { convertLegacyProps } from "antd/lib/button/button";
+import Loader from "./Loader";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -20,7 +21,7 @@ const News = ({ simplified }) => {
     newsCategory,
     count: simplified ? 6 : 12,
   });
-  if (!cryptoNews?.value) return "Loading...";
+  if (!cryptoNews?.value) return <Loader />;
   return (
     <Row gutter={[24, 24]}>
       {!simplified && (
