2b383d620c21856d30105d56c580462cf13aaa8d
Jacob Tan <jacob.tan229@gmail.com>
2021-12-21 18:45:29 +1300

    Fixed loader bug

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -33,7 +33,7 @@ function CryptoDetails() {
   });
   const cryptoDetails = data?.data?.coin;
 
-  if (isFetching) return "<Loader />";
+  if (isFetching) return <Loader />;
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
